e94fef4fe7cfaa1f2d74152934f7796d3518e323
Archit Sharma <architsh007@gmail.com>
2024-06-05 21:38:47 +0530

    .

---
+++ scrape_flipkart/scrape_flipkart/spiders/scarpe_laptops.py
@@ -0,0 +1,32 @@
+import scrapy
+from urllib.parse import urlencode
+
+API_key = 'ace1d4a22b164f3694148566b57fe2ba'
+
+def get_proxy_url(url):
+    country = "IN"
+    payload = {
+        'url' : url,
+        'x-api-key' : API_KEY,
+        'proxy_country' : country,
+        'browser' : 'false'
+    }
+    proxy_url = "https://api.scrapingant.com/v2/general?" + urlencode(payload)
+    return proxy_url
+
+from urllib.parse import urlencode
+API_KEY = "ace1d4a22b164f3694148566b57fe2ba"
+url = 'https://www.flipkart.com/computers/laptops/pr?sid=6bo,b5g&otracker=categorytree'
+
+
+
+proxy_url = "https://api.scrapingant.com/v2/general?" + urlencode(payload) + "&browser=false"
+print(str(proxy_url))
+
+class ScarpeLaptopsSpider(scrapy.Spider):
+    name = "scarpe_laptops"
+    allowed_domains = ["www.flipkart.com"]
+    start_urls = ["https://www.flipkart.com/computers/laptops/pr?sid=6bo,b5g&otracker=categorytree"]
+
+    def parse(self, response):
+        products = response
